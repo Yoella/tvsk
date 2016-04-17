1a8a4cb8e242a661a94f496f14122b89b8997c29
Yoella <joella.j@hotmail.com>
2016-04-17 15:47:55 +0200

    Start project

---
+++ hem.php
@@ -0,0 +1,18 @@
+<?php
+  include_once('main-elements/head.html');
+?>
+<div class="home-wrap">
+
+  <?php
+    include_once('main-elements/header.html');
+    include_once('main-elements/nav-1.html');
+    include_once('main-elements/nav-2.html');
+  ?>
+
+  <div class="hem-sections">
+    <?php
+      include_once('page-elements/hem.html');
+    ?>
+  </div>
+
+</div>
